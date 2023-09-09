a20e6f162e8ad7603ece841bce5e66f64323257a
Miguel Trujillo <miguel.trujillo@fleetcor.com>
2023-09-09 15:48:23 -0500

    Update NoFoundCategoryException message

--- src/main/java/mat/study/store/product/exeption/NoFoundCategoryException.java
+++ src/main/java/mat/study/store/product/exeption/NoFoundCategoryException.java
@@ -1,7 +1,7 @@
 package mat.study.store.product.exeption;
 
 public class NoFoundCategoryException extends RuntimeException {
-  private final static String MSG_BY_ID = "There is not any product with the name: ";
+  private final static String MSG_BY_ID = "There is not any category with the name: ";
 
   public NoFoundCategoryException(String name) {
     super(String.format(MSG_BY_ID + "%s", name));
